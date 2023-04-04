918cd85e63995014236c86f4c83451188bed7bdf
Mosserl <mosserl@unistra.fr>
2023-04-04 11:05:19 +0200

    A new service arrive allowing the user to add a camera to the printing process. The applications of such a node are simply to get a history of the printing process.

--- 3D_printer/src/ros2_3d_printer_image_capture/nodes/image_capture_node.py
+++ 3D_printer/src/ros2_3d_printer_image_capture/nodes/image_capture_node.py
@@ -1,28 +1,28 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
 
-from 3D_printer_messages.srv import profileCommand
-from 3D_printer_messages.srv import gcodeCommand
-from 3D_printer_messages.srv import imageCommand
+from ros2_3d_printer_messages.srv import ProfileCommand
+from ros2_3d_printer_messages.srv import GcodeCommand
+from ros2_3d_printer_messages.srv import ImageCommand
 import cv2 
 
 
 
 class ImageCaptureNode(Node):
         def __init__(self,cameraNumber = 0):
-                super().__init__('3D_printer_image_capture')
+                super().__init__('printer_image_capture')
                 self.webcam = cv2.VideoCapture("/dev/video"+str(cameraNumber)) # Be carefull with the number after video
-                self.imageService = self.create_service(imageCommand, 'ask_capture_image', self.takePicture)
+                self.imageService = self.create_service(ImageCommand, 'ask_capture_image', self.takePicture)
 
-               
         def __del__(self):
-                webcam.release()
+                self.webcam.release()
                 
         def takePicture(self,request, response):
-                check, frame = webcam.read()
-                cv2.imwrite(filename='/home/gulltor/Ramsai_Robotics/images/'+request.filename+'.jpg', img=frame)
-
+                check, frame = self.webcam.read()
+                cv2.imwrite(filename='/home/gulltor/Ramsai_Robotics/'+request.filename+'.jpg', img=frame)
+                self.get_logger().info('image acquired : /home/gulltor/Ramsai_Robotics/'+request.filename+'.jpg')
                 response.confirmation = True
                 return response
 
